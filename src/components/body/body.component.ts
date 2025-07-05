eda39240b1297021b7129486c207dfb8a62ea1b8
Gabriel <gersongabriel_@hotmail.com>
2025-07-05 02:28:22 -0300

    feat: move projetos para componente

--- src/components/body/body.component.ts
+++ src/components/body/body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InformacoesPessoaisComponent } from "./components/informacoes-pessoais/informacoes-pessoais.component";
+import { ProjetosComponent } from "./components/projetos/projetos.component";
 
 @Component({
   selector: 'app-body',
-  imports: [InformacoesPessoaisComponent],
+  imports: [InformacoesPessoaisComponent, ProjetosComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })

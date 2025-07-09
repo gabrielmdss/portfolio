488d46dbece9cb349db338f6f85b41e77b5305fd
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-07-09 13:59:13 -0300

    feat: transforma habilidades em components

--- src/components/body/body.component.ts
+++ src/components/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InformacoesPessoaisComponent } from "./components/informacoes-pessoais/informacoes-pessoais.component";
 import { ProjetosComponent } from "./components/projetos/projetos.component";
+import { HabilidadesComponent } from "./components/habilidades/habilidades.component";
 
 @Component({
   selector: 'app-body',
-  imports: [InformacoesPessoaisComponent, ProjetosComponent],
+  imports: [InformacoesPessoaisComponent, ProjetosComponent, HabilidadesComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })

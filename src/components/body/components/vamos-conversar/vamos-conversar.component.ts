98ea3c199930b5eedc865feef8b137fd154582b2
Gabriel <gersongabriel_@hotmail.com>
2025-07-30 22:15:00 -0300

    feat: bloco de vamos conversar

---
+++ src/components/body/components/vamos-conversar/vamos-conversar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vamos-conversar',
+  imports: [],
+  templateUrl: './vamos-conversar.component.html',
+  styleUrl: './vamos-conversar.component.css'
+})
+export class VamosConversarComponent {
+
+}

9a05896d35b23901074559f0a6f1ec701441d2d7
gabrielmdss <gersongabriel_@hotmail.com>
2025-07-31 23:30:31 -0300

    feat: adiciona links as sections da pagina

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  imports: [CommonModule],
   standalone: true,
   styleUrls: ['./header.component.css'],
 })
